c31e1f3c52dd1625df370b61b5fcd29ffe4a6b80
Narcissus2 <DMT1709533@xmu.edu.my>
2018-11-16 20:06:54 +0800

    Update Our Snack/include/Food.h

---
+++ Our Snack/include/Food.h
@@ -0,0 +1,18 @@
+#ifndef FOOD_H
+#define FOOD_H
+#include<utility>
+
+class Food
+{
+    public:
+        Food();
+        int foodx;
+        int foody;
+        virtual ~Food();
+    void print();
+    protected:
+
+    private:
+};
+
+#endif // FOOD_H
