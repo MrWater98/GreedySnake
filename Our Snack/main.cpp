3eecf881aa880743c1f1b8374c38bcc9ea767ed9
Narcissus2 <DMT1709533@xmu.edu.my>
2018-11-16 20:07:06 +0800

    Update Our Snack/main.cpp

---
+++ Our Snack/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<windows.h>
+#include "GameState.h"
+#include "Tool.h"
+using namespace std;
+int main()
+{
+    HideCursor();
+    GameState gameState;
+    return 0;
+}
