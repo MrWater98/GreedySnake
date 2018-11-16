6f15508728d931c6727ff9b649bf268bd809a282
Narcissus2 <DMT1709533@xmu.edu.my>
2018-11-16 20:07:01 +0800

    Update Our Snack/include/Snake.h

---
+++ Our Snack/include/Snake.h
@@ -0,0 +1,32 @@
+#ifndef SNAKE_H
+#define SNAKE_H
+#include<iostream>
+#include<deque>
+
+using namespace std;
+class Snake
+{
+    public:
+        Snake();
+        virtual ~Snake();
+        deque<pair<short,short> > snake;
+        int foodx;
+        int foody;
+        int direction = 77;
+        //把foodx和foody写成pair<short,short> food;
+        bool isOnSnake(pair<short,short> p);
+        int makeScore();
+        int getScore();
+        void print();
+        void printFood();
+        void foodCreate();
+        //把printFood和foodCreate合并
+        //int AIWalk(deque<pair<short,short> &snake,pair<short,short> food);
+        void walk();
+        bool check();
+    protected:
+
+    private:
+};
+
+#endif // SNAKE_H
